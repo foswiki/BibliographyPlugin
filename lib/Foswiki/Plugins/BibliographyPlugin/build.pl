18023e00697fad546369c6266b1da4546b965bf3
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/BibliographyPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/BibliographyPlugin/build.pl
+++ lib/Foswiki/Plugins/BibliographyPlugin/build.pl
@@ -3,16 +3,16 @@
 # Build for BibliographyPlugin
 #
 BEGIN {
-  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
-    unshift @INC, $pc;
-  }
+    foreach my $pc ( split( /:/, $ENV{FOSWIKI_LIBS} ) ) {
+        unshift @INC, $pc;
+    }
 }
 
 use Foswiki::Contrib::Build;
 
 # Create the build object
-my $build = new Foswiki::Contrib::Build( 'BibliographyPlugin' );
+my $build = new Foswiki::Contrib::Build('BibliographyPlugin');
 
 # Build the target on the command line, or the default target
-$build->build($build->{target});
+$build->build( $build->{target} );
 
